33c9e57cc73bd95ef01888d8237187ff07243114
Eric Nelson <nelseric@mail.gvsu.edu>
2012-02-22 16:21:55 -0500

    Adding base code

---
+++ SimpleSoccer/Teams/MyTeam/MySoccerTeam.cpp
@@ -0,0 +1,307 @@
+#include "MySoccerTeam.h"
+#include "../../SoccerPitch.h"
+#include "../../Goal.h"
+#include "../../PlayerBase.h"
+#include "../../GoalKeeper.h"
+#include "../../FieldPlayer.h"
+#include "misc/utils.h"
+#include "../../SteeringBehaviors.h"
+#include "../BucklandTeam/FieldPlayerStates.h"
+#include "../BucklandTeam/GoalKeeperStates.h"
+#include "../../ParamLoader.h"
+#include "2D/geometry.h"
+#include "Game/EntityManager.h"
+#include "Messaging/MessageDispatcher.h"
+#include "../../SoccerMessages.h"
+#include "MyTeamStates.h"
+#include "Debug/DebugConsole.h"
+#include <windows.h>
+using std::vector;
+
+
+MySoccerTeam::MySoccerTeam(Goal*        home_goal,
+                       Goal*        opponents_goal,
+                       SoccerPitch* pitch,
+					   team_color   color): AbstSoccerTeam(home_goal, opponents_goal, pitch, color)
+{
+	InitStateMachine();
+	CreatePlayers();
+	RegisterPlayers();
+	InitPlayers();
+
+  //create the sweet spot calculator
+  m_pSupportSpotCalc = new SupportSpotCalculator(Prm.NumSupportSpotsX,
+                                                 Prm.NumSupportSpotsY,
+                                                 this);
+}
+
+
+//----------------------- dtor -------------------------------------------
+//
+//------------------------------------------------------------------------
+MySoccerTeam::~MySoccerTeam()
+{
+
+  delete m_pSupportSpotCalc;
+}
+
+void MySoccerTeam::InitStateMachine()
+{
+  m_pStateMachine = new StateMachine<AbstSoccerTeam>(this);
+
+  m_pStateMachine->SetCurrentState(MyDefending::Instance());
+  m_pStateMachine->SetPreviousState(MyDefending::Instance());
+  m_pStateMachine->SetGlobalState(NULL);
+}
+
+void MySoccerTeam::InitPlayers()
+{
+  //set default steering behaviors
+  std::vector<PlayerBase*>::iterator it = m_Players.begin();
+
+  for (it; it != m_Players.end(); ++it)
+  {
+    (*it)->Steering()->SeparationOn();   
+  }
+}
+
+
+
+//------------------------- CreatePlayers --------------------------------
+//
+//  creates the players
+//------------------------------------------------------------------------
+void MySoccerTeam::CreatePlayers()
+{
+  if (Color() == blue)
+  {
+    //goalkeeper
+    m_Players.push_back(new GoalKeeper(this,
+                               1,
+                               TendGoal::Instance(),
+							   GlobalKeeperState::Instance(),
+                               Vector2D(0,1),
+                               Vector2D(0.0, 0.0),
+                               Prm.PlayerMass,
+                               Prm.PlayerMaxForce,
+                               Prm.PlayerMaxSpeedWithoutBall,
+                               Prm.PlayerMaxTurnRate,
+                               Prm.PlayerScale));
+ 
+    //create the players
+    m_Players.push_back(new FieldPlayer(this,
+                               6,
+                               Wait::Instance(),
+							   GlobalPlayerState::Instance(),
+                               Vector2D(0,1),
+                               Vector2D(0.0, 0.0),
+                               Prm.PlayerMass,
+                               Prm.PlayerMaxForce,
+                               Prm.PlayerMaxSpeedWithoutBall,
+                               Prm.PlayerMaxTurnRate,
+                               Prm.PlayerScale,
+                               PlayerBase::attacker));
+
+
+
+        m_Players.push_back(new FieldPlayer(this,
+                               8,
+                               Wait::Instance(),
+                               GlobalPlayerState::Instance(),
+                               Vector2D(0,1),
+                               Vector2D(0.0, 0.0),
+                               Prm.PlayerMass,
+                               Prm.PlayerMaxForce,
+                               Prm.PlayerMaxSpeedWithoutBall,
+                               Prm.PlayerMaxTurnRate,
+                               Prm.PlayerScale,
+                               PlayerBase::attacker));
+
+
+ 
+
+
+        m_Players.push_back(new FieldPlayer(this,
+                               3,
+                               Wait::Instance(),
+                               GlobalPlayerState::Instance(),
+                               Vector2D(0,1),
+                               Vector2D(0.0, 0.0),
+                               Prm.PlayerMass,
+                               Prm.PlayerMaxForce,
+                               Prm.PlayerMaxSpeedWithoutBall,
+                               Prm.PlayerMaxTurnRate,
+                               Prm.PlayerScale,
+                               PlayerBase::defender));
+
+
+        m_Players.push_back(new FieldPlayer(this,
+                               5,
+                               Wait::Instance(),
+                               GlobalPlayerState::Instance(),
+                               Vector2D(0,1),
+                               Vector2D(0.0, 0.0),
+                               Prm.PlayerMass,
+                               Prm.PlayerMaxForce,
+                               Prm.PlayerMaxSpeedWithoutBall,
+                               Prm.PlayerMaxTurnRate,
+                               Prm.PlayerScale,
+                              PlayerBase::defender));
+
+  }
+
+  else
+  {
+
+     //goalkeeper
+    m_Players.push_back(new GoalKeeper(this,
+                               16,
+                               TendGoal::Instance(),
+                               GlobalKeeperState::Instance(),
+                               Vector2D(0,-1),
+                               Vector2D(0.0, 0.0),
+                               Prm.PlayerMass,
+                               Prm.PlayerMaxForce,
+                               Prm.PlayerMaxSpeedWithoutBall,
+                               Prm.PlayerMaxTurnRate,
+                               Prm.PlayerScale));
+
+
+    //create the players
+    m_Players.push_back(new FieldPlayer(this,
+                               9,
+                               Wait::Instance(),
+                               GlobalPlayerState::Instance(),
+                               Vector2D(0,-1),
+                               Vector2D(0.0, 0.0),
+                               Prm.PlayerMass,
+                               Prm.PlayerMaxForce,
+                               Prm.PlayerMaxSpeedWithoutBall,
+                               Prm.PlayerMaxTurnRate,
+                               Prm.PlayerScale,
+                               PlayerBase::attacker));
+
+    m_Players.push_back(new FieldPlayer(this,
+                               11,
+                               Wait::Instance(),
+                               GlobalPlayerState::Instance(),
+                               Vector2D(0,-1),
+                               Vector2D(0.0, 0.0),
+                               Prm.PlayerMass,
+                               Prm.PlayerMaxForce,
+                               Prm.PlayerMaxSpeedWithoutBall,
+                               Prm.PlayerMaxTurnRate,
+                               Prm.PlayerScale,
+                               PlayerBase::attacker));
+
+
+ 
+    m_Players.push_back(new FieldPlayer(this,
+                               12,
+                               Wait::Instance(),
+                               GlobalPlayerState::Instance(),
+                               Vector2D(0,-1),
+                               Vector2D(0.0, 0.0),
+                               Prm.PlayerMass,
+                               Prm.PlayerMaxForce,
+                               Prm.PlayerMaxSpeedWithoutBall,
+                               Prm.PlayerMaxTurnRate,
+                               Prm.PlayerScale,
+                               PlayerBase::defender));
+
+
+    m_Players.push_back(new FieldPlayer(this,
+                               14,
+                               Wait::Instance(),
+                               GlobalPlayerState::Instance(),
+                               Vector2D(0,-1),
+                               Vector2D(0.0, 0.0),
+                               Prm.PlayerMass,
+                               Prm.PlayerMaxForce,
+                               Prm.PlayerMaxSpeedWithoutBall,
+                               Prm.PlayerMaxTurnRate,
+                               Prm.PlayerScale,
+                               PlayerBase::defender));
+                      
+  }
+}
+
+void MySoccerTeam::prepareForKickoff ()
+{
+	GetFSM()->ChangeState(MyPrepareForKickOff::Instance());
+}
+
+//---------------------- UpdateTargetsOfWaitingPlayers ------------------------
+//
+//  
+void MySoccerTeam::UpdateTargetsOfWaitingPlayers()const
+{
+  std::vector<PlayerBase*>::const_iterator it = m_Players.begin();
+
+  for (it; it != m_Players.end(); ++it)
+  {  
+    if ( (*it)->Role() != PlayerBase::goal_keeper )
+    {
+      //cast to a field player
+      FieldPlayer* plyr = static_cast<FieldPlayer*>(*it);
+      
+      if ( plyr->GetFSM()->isInState(*Wait::Instance()) ||
+           plyr->GetFSM()->isInState(*ReturnToHomeRegion::Instance()) )
+      {
+        plyr->Steering()->SetTarget(plyr->HomeRegion()->Center());
+      }
+    }
+  }
+}
+  //------------------------ CanShoot --------------------------------------
+//
+//  Given a ball position, a kicking power and a reference to a vector2D
+//  this function will sample random positions along the opponent's goal-
+//  mouth and check to see if a goal can be scored if the ball was to be
+//  kicked in that direction with the given power. If a possible shot is 
+//  found, the function will immediately return true, with the target 
+//  position stored in the vector ShotTarget.
+//------------------------------------------------------------------------
+bool MySoccerTeam::CanShoot(Vector2D  BallPos,
+                          double     power, 
+                          Vector2D& ShotTarget)const        // GT 2/10/10 Override
+{
+  //the number of randomly created shot targets this method will test
+  //GT 2/10/10 the number has been multiplied by three relative to the Buckland team
+  int NumAttempts = Prm.NumAttemptsToFindValidStrike*3; 
+
+  while (NumAttempts--)
+  {
+    //choose a random position along the opponent's goal mouth. (making
+    //sure the ball's radius is taken into account)
+    ShotTarget = OpponentsGoal()->Center();
+
+    //the y value of the shot position should lay somewhere between two
+    //goalposts (taking into consideration the ball diameter)
+    int MinYVal = OpponentsGoal()->LeftPost().y + Pitch()->Ball()->BRadius();
+    int MaxYVal = OpponentsGoal()->RightPost().y - Pitch()->Ball()->BRadius();
+
+    ShotTarget.y = (double)RandInt(MinYVal, MaxYVal);
+
+    //make sure striking the ball with the given power is enough to drive
+    //the ball over the goal line.
+    double time = Pitch()->Ball()->TimeToCoverDistance(BallPos,
+                                                      ShotTarget,
+                                                      power);
+    
+    //if it is, this shot is then tested to see if any of the opponents
+    //can intercept it.
+    if (time >= 0)
+    {
+      if (isPassSafeFromAllOpponents(BallPos, ShotTarget, NULL, power))
+      {
+        return true;
+      }
+    }
+  }
+  
+  return false;
+}
+
+
+
